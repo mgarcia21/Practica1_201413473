5df48463e819bfdaf35ed446319e749bdc31c7d7
Maynor Garcia <EADE@IT>
2017-08-21 20:08:08 -0600

    Bienvenida creada

--- src/buscaMinas_201413473/buscaMinas_201413473.java
+++ src/buscaMinas_201413473/buscaMinas_201413473.java
@@ -7,4 +7,12 @@ package buscaMinas_201413473;
  */
 public class buscaMinas_201413473 {
     
+   /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Maynor Rolando Garcia Castro \n201413473 \nIPC 'A' Practica 1 \nBuscaminas!");
+    }
+    
 }
